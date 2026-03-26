669da14263a150d3a016544699d0402427000972
danechka <danchik3859@gmail.com>
2026-03-26 11:10:15 +0300

    ÑСоздал классы Qustieon Ии TypeQuestion

---
+++ ExamPaperFactory/Question.cs
@@ -0,0 +1,28 @@
+﻿namespace ExamPaperFactory;
+
+public class Question
+{
+    private string _question { get; init; }
+    private TypeQuestion _type { get; init; }
+
+    public Question(string question, TypeQuestion type)
+    {
+        if (question == "")
+        {
+            throw new Exception("Вопрос не должен быть пустым");
+        }
+        else
+        {
+            _question = question;
+        }
+
+        if (type == null)
+        {
+            throw new Exception(" Тип не может быть пустым");
+        }
+        else
+        {
+            _type = type;
+        }
+    }
+}
---
+++ ExamPaperFactory/TypeQuestion.cs
@@ -0,0 +1,21 @@
+﻿namespace ExamPaperFactory;
+
+public class TypeQuestion
+{
+    
+    private string Name { get; init; }
+    private string? Description { get; init; }
+
+    public TypeQuestion(string name, string? description)
+    {
+        if (name == "")
+        {
+            throw new Exception("имя не может быть пустым");
+        }
+        else
+        {
+            Name = name;
+        }
+        Description = description;
+    }
+}
