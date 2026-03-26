ac9078fe7e016e3e9efa0087ff90dceb6c249d65
The-Blight <hezer.fil@gmail.com>
2026-03-26 11:50:46 +0300

    Сделал контрактр для правил ппроверки валидации

---
+++ Validation.CoreLib/Core/IValidationRule.cs
@@ -0,0 +1,16 @@
+﻿namespace Validation.CoreLib.Core;
+
+/// <summary>
+/// Определяет контракт для реализации правил проверки валидации.
+/// </summary>
+/// <typeparam name="T">Тип объекта, который подлежит проверке.</typeparam>
+public interface IValidationRule <T>
+{
+
+    /// <summary>
+    /// Выполняет проверку переданного объекта.
+    /// </summary>
+    /// <param name="item">Объект для проверки.</param>
+    /// <returns>Результат валидации с возможными ошибками.</returns>
+    ValidationResult Validate(T item); 
+}
