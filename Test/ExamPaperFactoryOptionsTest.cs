bd9c37967c9387a8e1412d55b5ad21f35157dc9c
Komar4iKs <komarikartem886@gmail.com>
2026-03-26 11:42:18 +0300

    create ExamPaperFactoryOptionsTest

---
+++ Test/ExamPaperFactoryOptionsTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Test;
+
+public class ExamPaperFactoryOptionsTest
+{
+    [Fact]
+    public void Constructor_SetsQuantityQuestionsCorrectly()
+    {
+        // Arrange
+        const int expectedQuantity = 10;
+
+        // Act
+        var options = new ExamPaperFactoryOptions(expectedQuantity);
+
+        // Assert
+        Assert.Equal(expectedQuantity, options.QuantityQuestions);
+    }
+
+}
