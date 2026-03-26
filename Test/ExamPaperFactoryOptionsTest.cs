cc9ef95eb27939742bebb54e431b991ba881183d
emo <ivan.galkin087@gmail.com>
2026-03-26 13:28:53 +0300

    create ExamPaperFactoryOptionsTest

---
+++ Test/ExamPaperFactoryOptionsTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Test;
+
+public class ExamPaperFactoryOptionsTest
+{
+    public class ExamPaperFactoryOptionsTest
+    {
+        [Fact]
+        public void Constructor_SetsQuantityQuestionsCorrectly()
+        {
+            const int expectedQuantity = 10;
+            
+            var options = new ExamPaperFactoryOptions(expectedQuantity);
+            
+            Assert.Equal(expectedQuantity, options.QuantityQuestions);
+        }
+    }
+}
