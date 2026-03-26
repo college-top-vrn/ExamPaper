e0c7ee019659724bc205153647da5c255cd27d01
FizzY650 <bloodw629@gmail.com>
2026-03-26 11:58:01 +0300

    проверка текста

---
+++ Validation.CoreLib/UniqueQuestionRule.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+
+public class Question
+{
+    public string Text { get; set; }
+}
+
+public class UniqueQuestionRule
+{
+    public bool IsValid(Question[] question)
+    {
+        if (question == null)
+            return true;
+        
+        return question 
+            .Where(q => q != null && !string.IsNullOrWhiteSpace(q.Text))
+            .Select(q => q.Text.Trim())
+            .GroupBy(t => t)
+            .All(g => g.Count() == 1);
+    }
+}
