34512d0554f188e108a51eb6f4fb566ab191e703
emo <ivan.galkin087@gmail.com>
2026-03-26 11:05:45 +0300

    create TypeQuestionTest

---
+++ Test/TypeQuestionTest.cs
@@ -0,0 +1,35 @@
+﻿namespace Test;
+
+public class TypeQuestionTest
+{
+    [Fact]
+    public void Test1()
+    {
+        [Fact]
+        public void Constructor_WithOnlyName_CreatesTypeQuestion()
+        {
+
+            var typeQuestion = new TypeQuestion("Multiple Choice");
+        
+            Assert.Equal("Multiple Choice", typeQuestion.Name);
+        }
+    
+        [Fact]
+        public void Constructor_WithOnlyDecsprition_CreatesTypeQuestion()
+        {
+
+            var typeQuestion = new TypeQuestion("Multiple Choice", "No Comments");
+        
+            Assert.Equal("No Comments", typeQuestion.Descprition);
+        }
+    
+        [Fact]
+        public void Constructor_WithOnlyDecspritionNull_CreatesTypeQuestion()
+        {
+
+            var typeQuestion = new TypeQuestion("Multiple Choice");
+        
+            Assert.Null(typeQuestion.Descprition);
+        }
+    }
+}
