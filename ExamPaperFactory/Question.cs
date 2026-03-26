908a0e37bce4e4e7896f926f3f838db0989a07e5
danechka <danchik3859@gmail.com>
2026-03-26 12:10:08 +0300

    Fix

--- ExamPaperFactory/Question.cs
+++ ExamPaperFactory/Question.cs
@@ -2,27 +2,12 @@
 
 public class Question
 {
-    private string _question { get; init; }
-    private TypeQuestion _type { get; init; }
+    public string _question { get; init; }
+    public TypeQuestion _type { get; init; }
 
     public Question(string question, TypeQuestion type)
     {
-        if (question == "")
-        {
-            throw new Exception("Вопрос не должен быть пустым");
-        }
-        else
-        {
-            _question = question;
-        }
-
-        if (type == null)
-        {
-            throw new Exception(" Тип не может быть пустым");
-        }
-        else
-        {
-            _type = type;
-        }
+        _question = question;
+        _type = type;
     }
 }
--- ExamPaperFactory/TypeQuestion.cs
+++ ExamPaperFactory/TypeQuestion.cs
@@ -3,19 +3,12 @@
 public class TypeQuestion
 {
     
-    private string Name { get; init; }
-    private string? Description { get; init; }
+    public string Name { get; init; }
+    public string? Description { get; init; }
 
     public TypeQuestion(string name, string? description)
     {
-        if (name == "")
-        {
-            throw new Exception("имя не может быть пустым");
-        }
-        else
-        {
-            Name = name;
-        }
+        Name = name;
         Description = description;
     }
 }
