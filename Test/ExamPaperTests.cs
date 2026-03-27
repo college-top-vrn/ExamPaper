ea09500c4fc0135450af58d86c11a43bfe693897
Komar4iKs <komarikartem886@gmail.com>
2026-03-27 11:49:36 +0300

    rebuilding ExamPaperTests class

--- Test/ExamPaperTests.cs
+++ Test/ExamPaperTests.cs
@@ -2,45 +2,50 @@
 
 public class ExamPaperTests
 {
+   
     [Fact]
-    public void Constructor_WithNumberAndQuestions_CreatesExamPaper()
+    public void Constructor_Sets_Number_Correctly()
     {
-        string number = "EXAM-001";
-        var questions = new List<Question>
-        {
-            new Question("Question 1", new TypeQuestion("Multiple Choice")),
-            new Question("Question 2", new TypeQuestion("Essay"))
-        };
+        int expectedNumber = 42;
+        var questions = new List<Question>();
+        
+        var examPaper = new ExamPaper(expectedNumber, questions);
+        
+        Assert.Equal(expectedNumber, examPaper.Number);
+    }
+    [Fact]
+    public void Constructor_Sets_Questions_Correctly()
+    {
+        var question = new Question("What is 2+2?", TypeQuestion.Math);
+        var expectedQuestions = new List<Question> { question };
+        
+        var examPaper = new ExamPaper(1, expectedQuestions);
+        
+        Assert.Equal(expectedQuestions.Count, examPaper.Questions.Count);
+        Assert.Equal(expectedQuestions[0].QuestionValue, examPaper.Questions[0].QuestionValue);
+    }
 
-        var examPaper = new ExamPaper(number, questions);
-
+    [Theory]
+    [InlineData(1)]
+    [InlineData(42)]
+    [InlineData(100)]
+    public void Constructor_Stores_Valid_Number(int number)
+    {
+        var examPaper = new ExamPaper(number, new List<Question>());
+        
         Assert.Equal(number, examPaper.Number);
-        Assert.Equal(questions, examPaper.Questions);
     }
 
     [Fact]
-    public void GetNumber_ReturnsExamNumber()
+    public void Questions_List_Is_ReadOnly_After_Construction()
     {
-        var examPaper = new ExamPaper("EXAM-2024-001", new List<Question>());
-
-        var number = examPaper.Number();
-
-        Assert.Equal("EXAM-2024-001", number);
-    }
-
-    [Fact]
-    public void GetQuestions_ReturnsQuestionsList()
-    {
-        var questions = new List<Question>
-        {
-            new Question("Q1", new TypeQuestion("Multiple Choice")),
-            new Question("Q2", new TypeQuestion("True/False"))
+        var questions = new List<Question> 
+        { 
+            new Question("Solve for x: 2x = 4", TypeQuestion.Math) 
         };
-        var examPaper = new ExamPaper("EXAM-001", questions);
-
-        var returnedQuestions = examPaper.Questions();
-
-        Assert.Equal(questions, returnedQuestions);
-        Assert.Equal(2, returnedQuestions.Count);
+        var examPaper = new ExamPaper(1, questions);
+        
+        Assert.Same(questions, examPaper.Questions);
+        Assert.Equal(1, examPaper.Questions.Count);
     }
 }
