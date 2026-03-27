718a35ce79561e349eeae25d5965fe8b1b1f8282
xRISOVARKAX <yle.do73777@gmail.com>
2026-03-27 11:19:38 +0300

    Добавил Документацию

--- ExamPaper.Core/ExamPaper/ExamPaperFactoryOption.cs
+++ ExamPaper.Core/ExamPaper/ExamPaperFactoryOption.cs
@@ -1,8 +1,18 @@
 ﻿namespace ExamPaper.Core.ExamPaper;
 
+/// <summary>
+/// Класс вопрос
+/// </summary>
 public class ExamPaperFactoryOption
 {
+    /// <summary>
+    /// Количество вопросов
+    /// </summary>
     public int QuantityQuestion { get; }
 
+    /// <summary>
+    /// Конструктор инициализирующий объекты
+    /// </summary>
+    /// <param name="quantityQuestion">Количество вопросов</param>
     public ExamPaperFactoryOption(int quantityQuestion) { QuantityQuestion = quantityQuestion; }
 }
