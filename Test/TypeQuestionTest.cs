839d4ced3fa4e837a1c26462b5b1a9b0a784ce25
emo <ivan.galkin087@gmail.com>
2026-03-27 10:54:43 +0300

    edit TypeQuestionTest

--- Test/TypeQuestionTest.cs
+++ Test/TypeQuestionTest.cs
@@ -1,11 +1,13 @@
-﻿namespace Test;
+﻿using ExamPaper;
+
+namespace Test;
 
 public class TypeQuestionTest
 {
     [Fact]
     public void Constructor_WithOnlyName_CreatesTypeQuestion()
     {
-        var typeQuestion = new TypeQuestion("Multiple Choice");
+        var typeQuestion = new TypeQuestion("Multiple Choice", null);
 
         Assert.Equal("Multiple Choice", typeQuestion.Name);
     }
@@ -15,14 +17,28 @@ public class TypeQuestionTest
     {
         var typeQuestion = new TypeQuestion("Multiple Choice", "No Comments");
 
-        Assert.Equal("No Comments", typeQuestion.Descprition);
+        Assert.Equal("No Comments", typeQuestion.Description);
     }
 
     [Fact]
     public void Constructor_WithOnlyDecspritionNull_CreatesTypeQuestion()
     {
-        var typeQuestion = new TypeQuestion("Multiple Choice");
+        var typeQuestion = new TypeQuestion("Multiple Choice", null);
 
-        Assert.Null(typeQuestion.Descprition);
+        Assert.Null(typeQuestion.Description);
+    }
+
+    [Theory]
+    [InlineData("Single Choice", "Single answer question")]
+    [InlineData("True/False", null)]
+    [InlineData("Matching", "Match items from two columns")]
+    [InlineData("Essay", "Long form answer")]
+    [InlineData("Rating", "Rate on a scale")]
+    public void Constructor_VariousValidInputs_SetsPropertiesCorrectly(string name, string? description)
+    {
+        var typeQuestion = new TypeQuestion(name, description);
+
+        Assert.Equal(name, typeQuestion.Name);
+        Assert.Equal(description, typeQuestion.Description);
     }
 }
