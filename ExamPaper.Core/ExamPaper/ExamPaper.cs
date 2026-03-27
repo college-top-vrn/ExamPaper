29bbab88f1eaffc5ced662d801fcce1f84f15ff6
ondraqsh <kond.6666666666@gmail.com>
2026-03-27 11:39:01 +0300

    ДоÐбавил авто документациюÐ

--- ExamPaper.Core/ExamPaper/ExamPaper.cs
+++ ExamPaper.Core/ExamPaper/ExamPaper.cs
@@ -1,10 +1,24 @@
 namespace ExamPaper.Core.ExamPaper;
 
+/// <summary>
+/// Класс экзаменационный лист
+/// </summary>
 public class ExamPaper
 {
+    /// <summary>
+    /// Номер билета
+    /// </summary>
     public int Number { get; }
+    /// <summary>
+    /// Список вопросов
+    /// </summary>
     public List<Question> Questions { get; }
-
+    
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="number">Номер</param>
+    /// <param name="questions">Список вопросов</param>
     public ExamPaper(int number, List<Question> questions)
     {
         Number = number;
--- ExamPaper.Core/ExamPaper/ExamPaperFactory.cs
+++ ExamPaper.Core/ExamPaper/ExamPaperFactory.cs
@@ -1,15 +1,30 @@
 namespace ExamPaper.Core.ExamPaper;
 
+/// <summary>
+/// Класс фабрика по созданию билетов
+/// </summary>
 public class ExamPaperFactory
 {
+    /// <summary>
+    /// Опция фабрики
+    /// </summary>
     public ExamPaperFactoryOption Options { get; }
+    /// <summary>
+    /// Список вопросов
+    /// </summary>
     public List<Question> Questions { get; }
-
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="options">Опция фабрики</param>
     public ExamPaperFactory(ExamPaperFactoryOption options)
     {
         Options = options;
     }
-
+    /// <summary>
+    /// Метод создания билета
+    /// </summary>
+    /// <returns>Новый эекзаменационный билет</returns>
     public ExamPaper Create()
     {
         return new ExamPaper(Options.QuantityQuestion, Questions);
