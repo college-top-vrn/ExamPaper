4e2748ba363da766740637781cf2aefd9aa8f7f0
danechka <danchik3859@gmail.com>
2026-03-27 11:02:59 +0300

    Documentation

--- ExamPaper.Core/ExamPaper/Question.cs
+++ ExamPaper.Core/ExamPaper/Question.cs
@@ -1,10 +1,25 @@
 ﻿namespace ExamPaper.Core.ExamPaper;
 
+/// <summary>
+/// Класс вопрос
+/// </summary>
 public class Question
 {
+    /// <summary>
+    /// Вопрос
+    /// </summary>
     public string QuestionValue { get; init; }
+
+    /// <summary>
+    /// Описывает тип вопроса
+    /// </summary>
     public TypeQuestion Type { get; init; }
 
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="questionValue">Вопрос</param>
+    /// <param name="type">Тип вопроса</param>
     public Question(string questionValue, TypeQuestion type)
     {
         QuestionValue = questionValue;
--- ExamPaper.Core/ExamPaper/TypeQuestion.cs
+++ ExamPaper.Core/ExamPaper/TypeQuestion.cs
@@ -1,10 +1,25 @@
 ﻿namespace ExamPaper.Core.ExamPaper;
 
+/// <summary>
+/// Класс отвечающий за тип вопроса
+/// </summary>
 public class TypeQuestion
 {
+    /// <summary>
+    /// Имя
+    /// </summary>
     public string Name { get; init; }
+
+    /// <summary>
+    /// Описывает тип вопроса
+    /// </summary>
     public string? Description { get; init; }
 
+    /// <summary>
+    /// Конструктор инициализирующий объекты
+    /// </summary>
+    /// <param name="name">Имя</param>
+    /// <param name="description">Описывает тип вопроса</param>
     public TypeQuestion(string name, string? description)
     {
         Name = name;
